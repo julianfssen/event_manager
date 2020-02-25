062466d7f0f92af56b8a7543adedf876ad6817eb
julianfoo <julianfssen@gmail.com>
2020-02-24 21:47:01 -0500

    Added function to check incomplete/missing zipcodes to 5 digits

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,24 @@
+require "csv"
 puts "Event Manager Initialized"
 
-lines = File.readlines "event_attendees.csv"
-lines.each_with_index do |line, index|
-    next if index == 0
-    columns = line.split(',')
-    name = columns[2]
-    puts name
+def clean_zipcode(zip)
+    if zip.nil?
+        zip = '00000'
+    elsif zip.length > 5
+        zip = zip[0..4]
+    elsif zip.length < 5
+        zip = zip.rjust 5, "0"
+    else
+        zip
+    end
+end
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+    name = row[:first_name]
+    zip = row[:zipcode]
+
+    zip = clean_zipcode(zip)
+
+    puts "#{name}, #{zip}"
 end
